24a0e2796f225d8abb73022e1d8a19cc7fce70c4
imran-binhasan <imranbinhasan.work@gmail.com>
2024-11-26 03:30:48 +0600

    initialize

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './Root.jsx'
-import Users from './Users.jsx'
 const router = createBrowserRouter([
   {
   path:'/',
